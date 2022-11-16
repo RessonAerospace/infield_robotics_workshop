29f4c5668a0f9309dd1ba48075031622e0a65911
Tjark Schuette <tschuette@atb-potsdam.de>
2022-11-16 21:30:38 +0100

    changed package name to reflect repository name

--- scripts/task2.py
+++ scripts/task2.py
@@ -23,7 +23,7 @@ class RfidReader():
     def __init__(self):        
         # get path of ros_pkg
         rospack = rospkg.RosPack()
-        pkg_folder = rospack.get_path('infield_robotics_ws')
+        pkg_folder = rospack.get_path('infield_robotics_workshop')
         
         # open a csv file in the results folder of the package for data saving
         self.fh = open(pkg_folder + "/results/humidity_sensors.csv", "w")
